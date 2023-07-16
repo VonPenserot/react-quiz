0b489a9229f431ac756cd9bf79723de9f8872a4e
Joachim da Silva Penserot <joachim.silva.penserot@gmail.com>
2023-07-16 11:51:05 -0400

    finished project

---
+++ src/components/Options.js
@@ -0,0 +1,23 @@
+export default function Options({ question, answer, dispatch }) {
+  const hasAnswered = answer !== null;
+  return (
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button
+          key={option}
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          disabled={answer !== null}
+          onClick={() => dispatch({ type: "newAnswer", payload: +index })}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
